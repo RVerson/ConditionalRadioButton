7f52f30d04add81477571df93264fe8c50f869d6
RVerson <reinaldo.verson@icloud.com>
2017-09-24 22:11:17 -0400

    Add project files.

---
+++ ChallengeConditionalRadioButton1/Default.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengeConditionalRadioButton1.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Your Note Taking Preferences&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <br />
+        </div>
+        <p>
+            <asp:RadioButton ID="pencilRadioButton" runat="server" GroupName="Group1" OnCheckedChanged="RadioButton1_CheckedChanged" Text="Pencil" />
+&nbsp;<br />
+            <asp:RadioButton ID="penRadioButton" runat="server" GroupName="Group1" OnCheckedChanged="penRadioButton_CheckedChanged" Text="Pen" />
+&nbsp;<br />
+            <asp:RadioButton ID="phoneRadioButton" runat="server" GroupName="Group1" Text="Phone" />
+&nbsp;
+            <br />
+            <asp:RadioButton ID="tabletRadioButton" runat="server" GroupName="Group1" Text="Tablet" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        </p>
+        <p>
+            &nbsp;</p>
+        <p>
+            <asp:Button ID="okButton" runat="server" OnClick="okButton_Click" Text="OK" />
+        </p>
+        <p>
+            &nbsp;</p>
+        <p>
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+        </p>
+        <p>
+            &nbsp;</p>
+        <asp:Image ID="resultImage" runat="server" />
+    </form>
+</body>
+</html>
---
+++ ChallengeConditionalRadioButton1/Default.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeConditionalRadioButton1
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void RadioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void RadioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void okButton_Click(object sender, EventArgs e)
+        {
+            if (pencilRadioButton.Checked)
+            { resultLabel.Text = "You selected Pencil";
+                resultImage.ImageUrl = "pencil.png";
+            }
+            else if (penRadioButton.Checked)
+            { resultLabel.Text = "You selected Pen";
+                resultImage.ImageUrl = "pen.png";
+            }
+            else if (phoneRadioButton.Checked)
+            { resultLabel.Text = "You selected Phone";
+                resultImage.ImageUrl = "phone.png";
+            }
+            else if (tabletRadioButton.Checked)
+            { resultLabel.Text = "you selected Tablet";
+                resultImage.ImageUrl = "tablet.png";
+            }
+            else { resultLabel.Text = "Please Select an Option"; }
+            
+        }
+
+        protected void penRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
